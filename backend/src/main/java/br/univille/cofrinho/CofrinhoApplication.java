7adf52a6e114ee42573e2da0da632e98fcfe2570
Richard Bidin <richardbidin@outlook.com>
2023-04-15 14:34:11 -0300

    Adiciona swagger

--- backend/src/main/java/br/univille/cofrinho/CofrinhoApplication.java
+++ backend/src/main/java/br/univille/cofrinho/CofrinhoApplication.java
@@ -1,9 +1,17 @@
 package br.univille.cofrinho;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Cofrinho",
+		description = "Gerenciador financeiro"
+	)
+)
 public class CofrinhoApplication {
 
 	public static void main(String[] args) {
