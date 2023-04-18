36c4e67b0f52340efe121a1ccf5323db245ad699
Daslan <daslaanmateus.dev@gmail.com>
2023-04-17 22:16:38 -0300

    Renomeia atributo ordem para grandeza

--- backend/src/main/java/br/univille/cofrinho/domains/frequency/FrequenciaDTO.java
+++ backend/src/main/java/br/univille/cofrinho/domains/frequency/FrequenciaDTO.java
@@ -2,4 +2,4 @@ package br.univille.cofrinho.domains.frequency;
 
 import java.util.UUID;
 
-public record FrequenciaDTO (UUID id, String nome, int ordem) {}
+public record FrequenciaDTO (UUID id, String nome, int grandeza) {}
