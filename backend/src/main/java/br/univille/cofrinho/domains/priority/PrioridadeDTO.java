4dbbee692d96d644dbc39b8d3021decbbfb9776c
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-04-17 22:21:04 -0300

    Renomeia atributo ordem para grandeza (#10)

--- backend/src/main/java/br/univille/cofrinho/domains/priority/PrioridadeDTO.java
+++ backend/src/main/java/br/univille/cofrinho/domains/priority/PrioridadeDTO.java
@@ -2,4 +2,4 @@ package br.univille.cofrinho.domains.priority;
 
 import java.util.UUID;
 
-public record PrioridadeDTO (UUID id, String nome, int ordem) {}
+public record PrioridadeDTO (UUID id, String nome, int grandeza) {}
