737bdb5a0216ef3abba3746c75b7a402465004c8
Richard Bidin <richardbidin@outlook.com>
2023-05-26 23:49:28 -0300

    Endpoint para listar contas bancarias (#60)

--- backend/src/main/java/br/univille/meuporquinho/domains/contabancaria/ContaBancariaRepository.java
+++ backend/src/main/java/br/univille/meuporquinho/domains/contabancaria/ContaBancariaRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ContaBancariaRepository extends JpaRepository<ContaBancariaEntity, UUID> {
+
+	@Query("SELECT c FROM ContaBancariaEntity c WHERE c.usuario = ?1")
+	List<ContaBancariaEntity> obterTodasPorUsuario(UsuarioEntity usuario);
 
 	@Query("SELECT c FROM ContaBancariaEntity c WHERE c.titulo = ?1 AND c.usuario = ?2")
 	Optional<ContaBancariaEntity> obterPorTituloEUsuario(String titulo, UsuarioEntity usuario);
